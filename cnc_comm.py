eabcfc6e9f0fa0057d7a489f08c3c766ea13f8e7
ionut <gorgosionut@yahoo.com>
2016-04-22 20:28:11 +0300

    Command line added

--- cnc_comm.py
+++ cnc_comm.py
@@ -2,35 +2,35 @@ import serial
 import serial.tools.list_ports
 import time
 
+
 class CncComm:
 
-	def __init__(self):
-		self.port = None
-		self.f = None
+    def __init__(self):
+        self.ser = None
+        self.f = None
 
 
-	def read_ports(self):
-		ports = list(serial.tools.list_ports.comports())
-		for p in ports:
-			self.port = serial.Serial(p.device, 115200)
-			# Open grbl serial port
+    def read_ports(self):
+        ports = list(serial.tools.list_ports.comports())
+        for p in ports:
+            self.ser = serial.Serial(p.device, 115200)   # Open the grbl port
 
+    def stream_code(self, file):
+        self.f = open(file,'r')
+        self.ser.write("\r\n\r\n")
+        time.sleep(2)
+        self.ser.flushInput()
 
-	def stream_code(self, file):
-		self.f = open(file,'r')
-		self.port.write("\r\n\r\n")
-		time.sleep(2)
-		self.port.flushInput()
+        # Stream g-code to grbl
+        for line in self.f:
+            l = line.strip()
+            print 'Sending: ' + l,
+            self.ser.write(l + '\n')
+            grbl_out = self.ser.readline()
+            print ' : ' + grbl_out.strip()
 
-		# Stream g-code to grbl
-		for line in self.f:
-			l = line.strip()
-			print 'Sending: ' + l,
-			self.port.write(l + '\n')
-			grbl_out = self.port.readline()
-			print ' : ' + grbl_out.strip()
+        raw_input("	Press <Enter> to exit and disable grbl.")
 
-		raw_input("	Press <Enter> to exit and disable grbl.")
-		
-		self.f.close()
-		self.port.close()
+        self.f.close()
+        self.ser.close()
+
--- stream.py
+++ stream.py
@@ -1,5 +1,22 @@
+import argparse
 import cnc_comm
 
-cnc = cnc_comm.CncComm()
-cnc.read_ports()
-cnc.stream_code('grbl.gcode')
+def main():
+    
+# Command line tool
+
+    parser = argparse.ArgumentParser(description='Help')
+    parser.add_argument(
+        'grbl_file',
+        help='the file containing the gcode')
+
+    args = parser.parse_args()
+
+    filename = args.grbl_file
+
+    cnc = cnc_comm.CncComm()
+    cnc.read_ports()
+    cnc.stream_code(filename)
+
+if __name__ == '__main__':
+    main()
