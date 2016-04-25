7a71f4f9d21aec7bccdac86d21bf7578aed0b7db
ionut <gorgosionut@yahoo.com>
2016-04-25 14:02:39 +0300

    x_axis_movement

--- cnc_comm.py
+++ cnc_comm.py
@@ -33,7 +33,7 @@ class CncComm:
 
     def __init__(self):
         self.ser = None
-        self.f = None
+      #  self.xvalue = 0
 
 
     def read_ports(self):
@@ -41,23 +41,38 @@ class CncComm:
         for p in ports:
             self.ser = serial.Serial(p.device, 115200)   # Open the grbl port
 
-    def stream_code(self, file):
-        self.f = open(file,'r')
+    def stream_code_from_file(self, file):
+        f = open(file,'r')
         self.ser.write("\r\n\r\n")
         time.sleep(2)
         self.ser.flushInput()
 
         # Stream g-code to grbl
-        for line in self.f:
+        for line in f:
             l = line.strip()
-            print 'Sending: ' + l,
+            print 'Sending: ' + l
             self.ser.write(l + '\n')
             grbl_out = self.ser.readline()
             print ' : ' + grbl_out.strip()
 
         raw_input("	Press <Enter> to exit and disable grbl.")
 
-        self.f.close()
+        f.close()
+        self.ser.close()
+
+    def stream_code_x_axis(self, xvalue):
+        self.ser.write("\r\n\r\n")
+        time.sleep(2)
+        self.ser.flushInput()
+
+        self.ser.write("$X\nG21\nG90" + '\n')
+        move = "G00 X" + xvalue
+        self.ser.write(move + '\n')
+        grbl_out = self.ser.readline()
+        print ' : ' + grbl_out.strip()
+
+        raw_input("	Press <Enter> to exit and disable grbl.")
+
         self.ser.close()
 
 def main():
@@ -68,13 +83,19 @@ def main():
         'grbl_file',
         help='the file containing the gcode')
 
+    parser.add_argument(
+        'xvalue',
+        help='the distance on x axis in mm')
+
     args = parser.parse_args()
 
     filename = args.grbl_file
+    xvalue = args.xvalue
 
     cnc = CncComm()
     cnc.read_ports()
-    cnc.stream_code(filename)
+    #cnc.stream_code_from_file(filename)
+   # cnc.stream_code_x_axis(xvalue)
 
 
 if __name__ == '__main__':
