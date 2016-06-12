c43a0a395bbe7f5cac224fa84ee2ab08978cbc85
IonutGorgos <gorgosionut@yahoo.com>
2016-06-12 03:00:53 +0300

    New options for command line added

--- cnc_comm.py
+++ cnc_comm.py
@@ -28,18 +28,17 @@ import serial
 import serial.tools.list_ports
 import time
 import argparse
+import sys
 
 class CncComm:
 
     def __init__(self):
         self.ser = None
-      #  self.xvalue = 0
+        self.speed = 2 # mm / s
 
 
-    def read_ports(self):
-        ports = list(serial.tools.list_ports.comports())
-        for p in ports:
-            self.ser = serial.Serial(p.device, 115200)   # Open the grbl port
+    def open_port(self, port):
+        self.ser = serial.Serial(port, 115200)   # Open the grbl port
 
     def stream_code_from_file(self, file):
         f = open(file,'r')
@@ -55,7 +54,7 @@ class CncComm:
             grbl_out = self.ser.readline()
             print ' : ' + grbl_out.strip()
 
-        raw_input("	Press <Enter> to exit and disable grbl.")
+        #raw_input("	Press <Enter> to exit and disable grbl.")
 
         f.close()
         self.ser.close()
@@ -64,15 +63,34 @@ class CncComm:
         self.ser.write("\r\n\r\n")
         time.sleep(2)
         self.ser.flushInput()
-
         self.ser.write("$X\nG21\nG90" + '\n')
-        move = "G00 X" + xvalue
+        move = "G00 X" + str(xvalue)
+        a = time.time()  # to measure time elapsed
         self.ser.write(move + '\n')
+        i = 0.0
+        while i < abs(xvalue):
+            print "X : " + str(i)
+            #print (str(i))
+            if (abs(xvalue) - i) < 0.005:
+                break
+            time.sleep(0.0525)
+            i = i + 0.105
+
+        elapsed = time.time() - a  # to measure time elapsed
+        print elapsed  # to measure time elapsed
         grbl_out = self.ser.readline()
-        print ' : ' + grbl_out.strip()
+        print 'X : ' + str(xvalue) + " = " + grbl_out.strip()
+        #raw_input("	Press <Enter> to exit and disable grbl."
+        self.ser.close()
 
-        raw_input("	Press <Enter> to exit and disable grbl.")
 
+    def go_home(self):
+        self.ser.write("\r\n\r\n")
+        time.sleep(2)
+        self.ser.flushInput()
+        self.ser.write("$X\nG21\nG90" + '\n')
+        self.ser.write("G00 X0 Y0 Z0")
+        #raw_input("Your position is (0, 0, 0), press <Enter> to exit.")
         self.ser.close()
 
 def main():
@@ -80,22 +98,42 @@ def main():
 
     parser = argparse.ArgumentParser(description='Help')
     parser.add_argument(
-        'grbl_file',
+        'port',
+        help = 'the name of the serial port to communicate to the Arduino, '
+               'e.g. COM10'
+    )
+    parser.add_argument(
+        '--f',
+        nargs=1,
+        type=argparse.FileType('r'),
+        default=False, metavar='filename',
         help='the file containing the gcode')
 
     parser.add_argument(
-        'xvalue',
+        '--x',
+        nargs = 1,
+        type = float,
+        default= 0.0,
+        metavar = 'value',
+        #action = 'store_true',
         help='the distance on x axis in mm')
+    parser.add_argument(
+        '--home',
+        action = 'store_true',
+        help='go to home (0, 0, 0)')
 
     args = parser.parse_args()
-
-    filename = args.grbl_file
-    xvalue = args.xvalue
+    port = args.port
 
     cnc = CncComm()
-    cnc.read_ports()
+    cnc.open_port(port)
+
+
+    #filename = args.grbl_file
+
     #cnc.stream_code_from_file(filename)
-   # cnc.stream_code_x_axis(xvalue)
+    cnc.stream_code_x_axis(args.x[0])
+    #cnc.go_home()
 
 
 if __name__ == '__main__':
